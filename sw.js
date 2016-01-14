13c5584e8d527581395731c0d23b6e55cafc30fe
Fumio SAGAWA <fumio.sagawa@gmail.com>
2016-01-14 13:10:03 +0900

    テスト

--- sw.js
+++ sw.js
@@ -27,6 +27,19 @@ this.addEventListener('install', function(event) {
   );
 });
 
+
+this.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (CACHE_NAME.indexOf(key) === -1) {
+          return caches.delete(key);
+        };
+      })
+    })
+  );
+});
+
 // fetch: Service Workersにキャッシュされたコンテンツに何かさせる
 // Service Worker がインストールされた状態で、
 // 他のページヘ移動したりページを更新したりすると、
@@ -37,15 +50,19 @@ this.addEventListener('fetch', function(event) {
   // HTTPレスポンスをハイジャックしてマジックを使って更新するために、
   // イベント上でrespondWith() メソッドを呼び出せます。
   event.respondWith(
+    // 使用可能なキャッシュと一致する場合
     caches.match(event.request)
-    .catch(function() {
-      return fetch(event.request).then(function(response) {
-        return caches.open(CACHE_NAME).then(function(cache) {
-          cache.put(event.request, response.clone());
-          return response;
-        });  
-      });
-    })
-    .
+      .catch(function() {
+        
+        return fetch(event.request).then(function(response) {
+          return caches.open(CACHE_NAME).then(function(cache) {
+            cache.put(event.request, response.clone());
+            return response;
+          });  
+        });
+      })
+      .catch(function() {
+        return caches.match('/sw-test/gallery/myLittleVader.jpg');
+      })
   );
 });
