d1c25a82e4d30df26b51f31217e13e514c6b7d9d
Fumio SAGAWA <fumio.sagawa@gmail.com>
2016-01-14 11:12:44 +0900

    テスト

--- sw.js
+++ sw.js
@@ -5,6 +5,7 @@ var path = '/offlineweb/';
 this.addEventListener('install', function(event) {
   
   console.log('Install Event Listener');
+  console.log('Install Event', event);
   
   event.waitUntil(
     caches.open('v1')
@@ -17,30 +18,30 @@ this.addEventListener('install', function(event) {
           path+'style.css',
           path+'app.js',
           path+'image-list.js',
-          path+'star-wars-logo.jpg',
-          path+'gallery/',
-          path+'gallery/bountyHunters.jpg',
-          path+'gallery/myLittleVader.jpg',
-          path+'gallery/snowTroopers.jpg'
+          path+'star-wars-logo.jpg'
         ]);
       })
   );
 });
 
 this.addEventListener('fetch', function(event) {
-  
   console.log('Fetch Event Listener');
   
   var response;
-  event.respondWith(caches.match(event.request).catch(function() {
-    return fetch(event.request);
-  }).then(function(r) {
-    response = r;
-    caches.open('v1').then(function(cache) {
-      cache.put(event.request, response);
-    });
-    return response.clone();
-  }).catch(function() {
-    return caches.match('/sw-test/gallery/myLittleVader.jpg');
-  }));
+  event.respondWith(
+    caches.match(event.request)
+      .catch(function() {
+        return fetch(event.request);
+      })
+      .then(function(r) {
+        response = r;
+        caches.open('v1')
+          .then(function(cache) {
+            cache.put(event.request, response);
+          });
+        return response.clone();
+      }).catch(function() {
+        return caches.match('/sw-test/gallery/myLittleVader.jpg');
+      })
+  );
 });
