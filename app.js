72d958b2698e0c1872058c2f337a564388cd6809
Fumio SAGAWA <fumio.sagawa@gmail.com>
2016-01-16 14:21:19 +0900

    てすと

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 // register service worker
 // http://albatrosary.github.io/offlineweb/
+(function () {
+  navigator
+    .serviceWorker
+    .getRegistration()
+    .then(function(registration) {
+      // service-worker.js が更新されたとき registration.update() でService Workerを更新する
+      console.log('registration');
+      return registration.update(); 
+    })
+    .then(function() {
+      // The script is updated, or there is no updated script.
+      console.log('The script is updated, or there is no updated script.');
+    })
+    .catch(function(e) {
+      // An error occurs during update (eg. Network error, Runtime error).
+      console.log('An error occurs during update (eg. Network error, Runtime error).');
+    });
+});
+
 var registration;
+// service-workerを登録
 function register() {
   // Service Workersがサポートされているか
   if ('serviceWorker' in navigator) {
@@ -26,26 +46,10 @@ function register() {
   }
 }
 
+// service-workerを削除
 function unregister() {
   registration.unregister()
     .then(function() {
       console.log('unregistered');
     });
 }
-
-navigator
-  .serviceWorker
-  .getRegistration()
-  .then(function(registration) {
-    // sw.js が更新されたとき registration.update() でService Workerを更新する
-    console.log('registration');
-    return registration.update(); 
-  })
-  .then(function() {
-    // The script is updated, or there is no updated script.
-    console.log('The script is updated, or there is no updated script.');
-  })
-  .catch(function(e) {
-    // An error occurs during update (eg. Network error, Runtime error).
-    console.log('An error occurs during update (eg. Network error, Runtime error).');
-  });
